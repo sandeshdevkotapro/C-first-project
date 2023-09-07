72fc3ba7a06213c81aa04a7eb5ccd80980454def
sande <sande@VEYGSAN>
2023-09-07 13:47:13 +0300

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public People(string name) {
+            this.Name = name;
+        }
+        public override string ToString()
+        {
+            return "Hello! THis is " + Name;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int total = 3;
+        People[] peoples = new People[total];
+        for(int i=0; i<total; i++)
+        {
+            peoples[i] = new People(Console.ReadLine());
+        }
+        for(int i=0; i<total;i++)
+        {
+            Console.WriteLine(peoples[i].ToString());
+        }
+    }
+}
